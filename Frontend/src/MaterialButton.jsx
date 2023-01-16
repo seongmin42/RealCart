8eb80710c1d98ab3bd1aee57f6a3a62e44a798f7
문성현 <forest7592@hanmail.net>
2023-01-16 17:29:37 +0900

    Feat: Add login screen
    consist of textfields and login,findPassword,startWithGoogle,join buttons
    
    Resolves: #S08P12A403-66
    
    Former-commit-id: 71c79cfbd9724301e40920cf2f2d75fc17d0e312

--- Frontend/src/MaterialButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Button } from "@mui/material";
-
-function MaterialButton(props) {
-  const data = props;
-  return <Button>{data.subject}</Button>;
-}
-
-export default MaterialButton;
