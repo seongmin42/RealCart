1b1ee1cae69459081d24c9545524a27ce61340cd
unknown <seongmin42@naver.com>
2023-01-16 11:53:56 +0900

    Feat: Create backend project
    create backend project with intellij
    
    
    Former-commit-id: 34826441991f62798d0524a613d1099baeeb6667

---
+++ realcart_backend/src/main/java/com/ssafy/realcart/RealcartApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.realcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealcartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealcartApplication.class, args);
+	}
+
+}
