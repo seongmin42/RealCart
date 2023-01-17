2a5fabb937d5a68521caf0e22c1fb3694eca906e
박성민 <seongmin42@naver.com>
2023-01-17 21:47:21 +0900

    Upload New File
    Former-commit-id: 9a9d92866b320fea763acc29960d2c224a1b716e

---
+++ STUDY/ParkSeongmin/serversocket.py
@@ -0,0 +1,17 @@
+import asyncio
+
+import websockets
+
+async def handler(websocket):
+    while True:
+        message = await websocket.recv()
+        print(message)
+
+
+async def main():
+    async with websockets.serve(handler, "", 8080):
+        await asyncio.Future()  # run forever
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
