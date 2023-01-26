d7dc2a2680c8eb12600e47653b34928bd2eb8e5f
forest7592 <forest7592@hanmail.net>
2023-01-27 01:29:06 +0900

    Feat: Add play waiting ui
    Former-commit-id: ea289ef84fba6c90c7a325ce4b7649b364626c82 [formerly 81bb0acd03ce62fd060214a92d94381c9178596a]
    Former-commit-id: f44f86593408733d7724938149bca5be3db77b86

---
+++ Frontend/src/components/InitialContent.jsx
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import PropTypes from "prop-types";
+import NewContent from "./NewContent";
+
+function InitialContent({
+  wait,
+  setWait,
+  handleModalOpen,
+  handleModalClose,
+  setIsReady,
+}) {
+  const [isInitial, setIsInitial] = useState(true);
+  return (
+    <Box>
+      {isInitial ? (
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            bgcolor: "background.paper",
+            width: "25%",
+            height: "25%",
+          }}
+        >
+          <Box
+            display="flex"
+            sx={{
+              width: "100%",
+              height: "55%",
+              justifyContent: "center",
+              alignItems: "end",
+            }}
+          >
+            <h2>현재 대기자 수는 {wait} 명입니다.</h2>
+          </Box>
+          <Box
+            display="flex"
+            sx={{
+              width: "100%",
+              height: "45%",
+            }}
+          >
+            <Box
+              display="flex"
+              sx={{
+                width: "50%",
+                height: "100%",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Button
+                variant="contained"
+                sx={{
+                  width: "50%",
+                  height: "35%",
+                  bgcolor: "white",
+                  color: "black",
+                }}
+                onClick={() => {
+                  setWait(wait + 1);
+                  setIsInitial(false);
+                }}
+              >
+                대기하기
+              </Button>
+            </Box>
+            <Box
+              display="flex"
+              sx={{
+                width: "50%",
+                height: "100%",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Button
+                variant="contained"
+                sx={{
+                  width: "50%",
+                  height: "35%",
+                  bgcolor: "white",
+                  color: "black",
+                }}
+                onClick={handleModalClose}
+              >
+                취소
+              </Button>
+            </Box>
+          </Box>
+        </Box>
+      ) : (
+        <NewContent
+          handleModalOpen={handleModalOpen}
+          handleModalClose={handleModalClose}
+          setIsReady={setIsReady}
+        />
+      )}
+    </Box>
+  );
+}
+
+InitialContent.propTypes = {
+  wait: PropTypes.number.isRequired,
+  setWait: PropTypes.func.isRequired,
+  handleModalOpen: PropTypes.func.isRequired,
+  handleModalClose: PropTypes.func.isRequired,
+  setIsReady: PropTypes.func.isRequired,
+};
+
+export default InitialContent;
