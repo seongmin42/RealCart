223f15c9cccc8b610875ea507c221162afd52fb9
문성현 <forest7592@hanmail.net>
2023-01-31 10:33:29 +0900

    Feat: Add content box and toolbar
    Former-commit-id: f6e5464767459a122ad89cef79c73d1c07835c2b [formerly 55e78ebc3f6da86d4425b711226478f41b5c5a17]
    Former-commit-id: 6bb8285785c8aa236075108525baf426eab2cdc9

--- Frontend/src/pages/freeboard/FreeBoardWrite.jsx
+++ Frontend/src/pages/freeboard/FreeBoardWrite.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Editor } from "react-draft-wysiwyg";
+import { EditorState, convertFromRaw } from "draft-js";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
+import AppButton from "../../components/AppButton";
 
 function FreeBoardWrite() {
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty()
+  );
+  useEffect(() => {
+    // console.log(editorState.getCurrentContent().getPlainText());
+    // console.log(convertFromRaw(editorState.getCurrentContent()));
+    convertFromRaw(editorState.getCurrentContent());
+  }, [editorState]);
   return (
     <Box
       sx={{
@@ -43,6 +55,7 @@ function FreeBoardWrite() {
         }}
       >
         <TextField
+          placeholder="제목을 입력하세요"
           sx={{
             width: "100%",
           }}
@@ -52,10 +65,75 @@ function FreeBoardWrite() {
         sx={{
           width: "100%",
           height: "75%",
-          bgcolor: "breen",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
         }}
       >
-        b
+        <Box
+          sx={{
+            width: "80%",
+            height: "80%",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Box
+            sx={{
+              width: "100%",
+              border: 1,
+              padding: "2px",
+              minHeight: "400px",
+            }}
+          >
+            <Editor
+              editorState={editorState}
+              onEditorStateChange={setEditorState}
+            />
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            width: "100%",
+            height: "20%",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Box
+            sx={{
+              width: "80%",
+              height: "30%",
+              display: "flex",
+              justifyContent: "end",
+              alignContent: "end",
+              marginTop: "30px",
+            }}
+          >
+            <Box flexGrow={1} />
+            <AppButton
+              sx={{
+                width: "100px",
+                height: "40px",
+                marginRight: "10px",
+                border: 1,
+              }}
+            >
+              취소
+            </AppButton>
+            <AppButton
+              sx={{
+                width: "100px",
+                height: "40px",
+                bgcolor: "black",
+                color: "white",
+              }}
+            >
+              등록
+            </AppButton>
+          </Box>
+        </Box>
       </Box>
     </Box>
   );
