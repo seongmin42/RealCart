15aef5ac3b9cbcb3c0141ce2761d46d56bc7b987
주해린 <06___what@naver.com>
2023-01-31 17:45:24 +0900

    FEAT: Add color sensing
    Added color,py
    
    Related to : #S08P12A403-158
    
    
    Former-commit-id: 38220f30fd7d4085ecb157102a392c3e03a903c7 [formerly 33df3b03dae1c5cdcfa0fbadba909367b5d9cc64]
    Former-commit-id: 2618ed31af9ed8433e4251aad178e7f11ef53184

--- IoT/RC_car_control/main.py
+++ IoT/RC_car_control/main.py
@@ -2,20 +2,27 @@ from DC_motor import DC_MOTOR
 from servo_motor import SERVO_MOTOR
 from car import CAR
 from ClientSocket import ClientSocket
+from color import COLOR
 
 def main():
     TCP_IP = '3.35.3.27'
     TCP_PORT = 8081
     
-    dc_enable = 14
+    dc_enable = 27
     dc_input_1 = 15
     dc_input_2 = 18
     
     servo_pin = 17
+
+    color_s2 = 23
+    color_s3 = 24
+    color_signal = 25
+    color_cycles = 10
     
     car_A = CAR()
     car_transmission = DC_MOTOR(dc_enable, dc_input_1, dc_input_2)
     car_handle = SERVO_MOTOR(servo_pin)
+    color = COLOR()
     
     client = ClientSocket(TCP_IP, TCP_PORT, car_A, car_transmission, car_handle)
 
