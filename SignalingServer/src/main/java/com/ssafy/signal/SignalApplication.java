f833d05f936f86f598c3825b10e0e4460ae0e8b5
SangWoo <brianpsw@naver.com>
2023-01-31 23:21:34 +0900

    Feat: Add ControllerAdvice Add ControllerAdvice which alert exception to mattermost Resolves: #S08P12A403-138
    Former-commit-id: b5d0903bea6a4a1bdb1be9253c12bb4fb603cbbd [formerly 13e7b4cc76be06d332e349b7bc7ee4aff61a0d64]
    Former-commit-id: 37338ecd1cfc0aca27b021e94f83f929106971ed

--- SignalingServer/src/main/java/com/ssafy/signal/SignalApplication.java
+++ SignalingServer/src/main/java/com/ssafy/signal/SignalApplication.java
@@ -4,20 +4,12 @@ import org.kurento.client.KurentoClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-@EnableWebSocket
+
 @SpringBootApplication
-public class SignalApplication implements WebSocketConfigurer{
+public class SignalApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SignalApplication.class, args);
-	}
-	
-	@Bean
-	public CallHandler callHandler() {
-		return new CallHandler();
 	}
 	
 	@Bean
@@ -25,10 +17,5 @@ public class SignalApplication implements WebSocketConfigurer{
 		return KurentoClient.create();
 	}
 	
-	@Override
-	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-		registry.addHandler(callHandler(), "/call").setAllowedOrigins("*");
-	}
-
 }
 
