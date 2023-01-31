69420fd10247c2b78a96a2dafcc3df824256a95b
Jeong UiGwon <jek9412@naver.com>
2023-01-31 09:29:35 +0900

    Feat: Add key down event function
    added key down event from web
    
    Related to: S08P12A403-150
    
    
    Former-commit-id: fb20fe0f97d0655015896a731b7be45e80dd2d4e [formerly 9cbd6b01ac4bd884a7a3dc346902cdbc88bdf12d]
    Former-commit-id: 9e709c9aa1dea4ba0a05bbdf6b7c6fc12c0ec719

---
+++ IoT/RC_car_control/car.py
@@ -0,0 +1,7 @@
+class CAR:
+    
+    def __init__(self):
+        
+        self.speed = 0
+        self.handle = 'center'
+        self.state = 0
---
+++ IoT/RC_car_control/main.py
@@ -0,0 +1,24 @@
+from DC_motor import DC_MOTOR
+from servo_motor import SERVO_MOTOR
+from car import CAR
+from ClientSocket import ClientSocket
+
+def main():
+    TCP_IP = '3.35.3.27'
+    TCP_PORT = 8081
+    
+    dc_enable = 14
+    dc_input_1 = 15
+    dc_input_2 = 18
+    
+    servo_pin = 17
+    
+    car_A = CAR()
+    car_transmission = DC_MOTOR(dc_enable, dc_input_1, dc_input_2)
+    car_handle = SERVO_MOTOR(servo_pin)
+    
+    client = ClientSocket(TCP_IP, TCP_PORT, car_A, car_transmission, car_handle)
+
+if __name__ == "__main__":    
+    main()
+
