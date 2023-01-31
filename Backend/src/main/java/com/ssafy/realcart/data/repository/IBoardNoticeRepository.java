7002dd2d90aaf11c66b061cf071f2a13205689ea
박상우 <brianpsw@naver.com>
2023-01-31 12:41:06 +0900

    Revert "Merge branch 'feat' into 'master'"
    This reverts merge request !27
    
    Former-commit-id: 2a2814e1d6a6261e35391cc577d9134a3190ac7e [formerly 6b028565f7ccd6b1c93216d482e5f2bebd7bb101]
    Former-commit-id: 8db2497a64b840a0e69ccc4267d4095f1f6d462f

--- Backend/src/main/java/com/ssafy/realcart/data/repository/IBoardNoticeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.realcart.data.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ssafy.realcart.data.entity.BoardNotice;
-
-public interface IBoardNoticeRepository extends JpaRepository<BoardNotice, Integer> {
-
-}
