32720315a865b5ecc623c7a5963cddb0867b5ef7
Jeong UiGwon <jek9412@naver.com>
2023-02-01 09:21:26 +0900

    Refactor: Refactor RC_car_control
    refactored RC_car_control python code
    
    Related to: S08P12A403-157
    
    
    Former-commit-id: fbf414955c79b44b98d5fe3b56bd212b8e60155d [formerly 0d6caeed42ed8cd4ec46e9e322d39efc4d19d4a4]
    Former-commit-id: 6a7de887d93384d39bcc855eac0f23986f2d79d5

--- IoT/RC_car_control/main.py
+++ IoT/RC_car_control/main.py
@@ -1,8 +1,29 @@
+import threading 
 from DC_motor import DC_MOTOR
 from servo_motor import SERVO_MOTOR
 from car import CAR
 from ClientSocket import ClientSocket
-from color import COLOR
+
+def driving():
+    try:
+        while True:
+            key_up = 38
+            key_down = 40
+            
+            if (self.car_A.command == key_up):
+                self.car_A.speed = self.car_A.speed + 10                
+                if (self.car_A.speed > 100): self.car_A.speed = 100
+            
+            if (self.car_A.command == key_down):
+                self.car_A.handle = 'center'
+                self.car_A.speed = self.car_A.speed - 10                
+                if (self.car_A.speed < -100): self.car_A.speed = -100
+            
+            car_transmission.drive(self.car_A.speed)
+    
+    except:
+        print('driving Error')
+            
 
 def main():
     TCP_IP = '3.35.3.27'
@@ -13,18 +34,15 @@ def main():
     dc_input_2 = 18
     
     servo_pin = 17
-
-    color_s2 = 23
-    color_s3 = 24
-    color_signal = 25
-    color_cycles = 10
     
     car_A = CAR()
-    car_transmission = DC_MOTOR(dc_enable, dc_input_1, dc_input_2)
+    car_transmission = DC_MOTOR(dc_enable, dc_input_1, dc_input_2)      
     car_handle = SERVO_MOTOR(servo_pin)
-    color = COLOR()
     
-    client = ClientSocket(TCP_IP, TCP_PORT, car_A, car_transmission, car_handle)
+    driving_thread = threading.Thread(target=driving)
+    
+    client = ClientSocket(TCP_IP, TCP_PORT, car_A)
+
 
 if __name__ == "__main__":    
     main()
