cd9e11b8a280fe21c8dcee428ba4c327ccf96fc3
정의권 <jek9412@naver.com>
2023-02-01 13:55:31 +0900

    Feat: Add serverSocket simulator
    added serverSocket simulator
    
    Realte to: S08P12A403-130
    
    Former-commit-id: 1092f4ed0f2ef82f186f23a76443f4e3c5da5f69 [formerly 4216e3aa036f5d2649f486c6d3e29568ad7daf61]
    Former-commit-id: 68a295741825750cf9fd0ff509c8fe7bf5c3703a

--- IoT/simulator/ServerSocket_simulator.py
+++ IoT/simulator/ServerSocket_simulator.py
@@ -1,29 +1,50 @@
 import socket
 from _thread import *
+from pynput import keyboard
 
 client_sockets = [] # 서버에 접속한 클라이언트 목록
+
+def on_press(key):
+    if (key == keyboard.Key.up):
+        client_socket.send("38".encode())
+    
+    if (key == keyboard.Key.down):
+        client_socket.send("40".encode())
+    
+    if (key == keyboard.Key.left):
+        client_socket.send("37".encode())
+    
+    if (key == keyboard.Key.right):
+        client_socket.send("39".encode())
+        
+    if (key == keyboard.Key.space):
+        client_socket.send("32".encode())
+
+def on_release(key):
+    print('Key %s released' %key)
+    if key == keyboard.Key.esc:
+        return False
 
 # 쓰레드에서 실행되는 코드입니다.
 # 접속한 클라이언트마다 새로운 쓰레드가 생성되어 통신을 하게 됩니다
 def threaded(client_socket, addr):
     print(">> Connected by:", addr[0], ':', addr[1])
 
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
+    
     # 클라이언트가 접속을 끊을 때 까지 반복합니다.
     while True:
 
         try:
             # 데이터가 수신되면 클라이언트에 다시 전송합니다.
-            # data = client_socket.recv(2)
+            data = client_socket.recv(2)
 
             if not data:
                 print(">> Disconnected by" + addr[0], ":", addr[1])
                 break
 
             print(">>Received from" + addr[0], ':', addr[1], data.decode())
-
-            # 서버에 접속한 클라이언트들에게 채팅 보내기
-            # 메세지를 보낸 본인을 제외한 서버에 접속한 클라이언트에게 메세지 보내기
-            client_socket.sendall("40".encode())
 
         except ConnectionResetError as e:
             print('>> Disconneted by' + addr[0], ':', addr[1])
@@ -48,9 +69,7 @@ server_socket.bind((HOST, PORT))
 server_socket.listen()
 
 # 클라이언트가 접속하면 accept 함수에서 새로운 소켓을 리턴합니다.
-
 # 새로운 쓰레드에서 해당 소켓을 사용하여 통신을 하게 됩니다.
-
 try:
     while True:
         print('>> Wait')
