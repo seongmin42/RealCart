b9b839a9799b308895bcd5b01bdffb95c62c70df
UiGwon Jeong <jek9412@naver.com>
2023-02-02 16:19:21 +0900

    Refactor: Refactor ClientSocket.py
    refactored Client Socket python code
    
    Related to: S08P12A403-140
    
    
    Former-commit-id: 85e7eb0cf9e6089dcafd7b3f946e9f85a9e93fb2 [formerly 6a3447cef9a1b69d73a6803db06a65836cb9bd5c]
    Former-commit-id: 7be1b3ecccef72e24d689cafba1e4f0f656c8374

--- IoT/RC_car_control/color.py
+++ IoT/RC_car_control/color.py
@@ -17,7 +17,6 @@ class COLOR:
             self.color_s2 = color_s2
             self.color_s3 = color_s3
             self.color_signal = color_signal
-            self.color_cycles = 10
             self.car_A = car_A
 
             print('Color Sensing GPIO pin Setting complete')
@@ -25,64 +24,69 @@ class COLOR:
         except:
             print('Color Sensing GPIO pin Setting failed')
 
-    def color_sensing(self, color_s2, color_s3, color_signal, color_cycles):
-
+    def color_sensing(self):
+        
         # color
         color_pink = [2000, 4000, 1000, 3000, 2000, 4000]  # red_min, red_max, green_min, green_max, blue_min, blue_max
         color_green = [550, 800, 1500, 2000, 800, 1100]
-
+        
+        color_cycles = 10
+        
         # Red
-        GPIO.output(color_s2, GPIO.LOW)
-        GPIO.output(color_s3, GPIO.LOW)
-        # time.sleep(0.1)
+        GPIO.output(self.color_s2, GPIO.LOW)
+        GPIO.output(self.color_s3, GPIO.LOW)
 
         start = time.time()
 
         for impulse_count in range(color_cycles):
-            GPIO.wait_for_edge(color_signal, GPIO.FALLING)
+            GPIO.wait_for_edge(self.color_signal, GPIO.FALLING)
 
         duration = time.time() - start  # seconds to rn for loop
         red = color_cycles / duration
 
 
         # Blue
-        GPIO.output(color_s2, GPIO.LOW)
-        GPIO.output(color_s3, GPIO.HIGH)
+        GPIO.output(self.color_s2, GPIO.LOW)
+        GPIO.output(self.color_s3, GPIO.HIGH)
         # time.sleep(0.1)
 
         start = time.time()
 
         for impulse_count in range(color_cycles):
-            GPIO.wait_for_edge(color_signal, GPIO.FALLING)
+            GPIO.wait_for_edge(self.color_signal, GPIO.FALLING)
 
         duration = time.time() - start  # seconds to rn for loop
         blue = color_cycles / duration
 
         # Green
-        GPIO.output(color_s2, GPIO.HIGH)
-        GPIO.output(color_s3, GPIO.HIGH)
+        GPIO.output(self.color_s2, GPIO.HIGH)
+        GPIO.output(self.color_s3, GPIO.HIGH)
         # time.sleep(0.1)
 
         start = time.time()
 
         for impulse_count in range(color_cycles):
-            GPIO.wait_for_edge(color_signal, GPIO.FALLING)
+            GPIO.wait_for_edge(self.color_signal, GPIO.FALLING)
 
         duration = time.time() - start  # seconds to rn for loop
         green = color_cycles / duration
 
+        # print('r, g, b:', red, blue, green)
+        
         if color_green[0] < red < color_green[1] and color_green[2] < green < color_green[3] and color_green[4] < blue < \
-                color_green[5] and self.car_A.state == 0:
+                color_green[5]:
             print('Ready')
             self.car_A.state = 1
             self.car_A.gate = 1
 
         elif color_pink[0] < red < color_pink[1] and color_pink[2] < green < color_pink[3] and color_pink[4] < blue < \
-                color_pink[5] and self.car_A.state == 1:
+                color_pink[5]:
             print('Goal')
-            self.car_A.speed = 0
-            DC_MOTOR.drive(self.car_A.speed)
             self.car_A.state = 0
             self.car_A.gate = 4
 
         return self.car_A.state
+    
+    def is_passing_gate(self):
+        while True:
+            self.color_sensing()
