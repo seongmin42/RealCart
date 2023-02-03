ec01eebad0452db538377b858db5d675453ee803
SangWoo <brianpsw@naver.com>
2023-02-03 13:24:21 +0900

    Fix: exclude actuator exclude actuator because of compatibility issue Related to: None
    Former-commit-id: a490d803c403dc736afdf6f353e27dc33cb35393 [formerly 86d5b55d1aa95a33d0ac858eb9e7338a19f48e6a]
    Former-commit-id: 82a7aa61e184c674de01839a96f6a3def9f0ee54

--- Backend/src/main/java/com/ssafy/realcart/RealcartApplication.java
+++ Backend/src/main/java/com/ssafy/realcart/RealcartApplication.java
@@ -3,7 +3,8 @@ package com.ssafy.realcart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+@EnableWebMvc
 @EnableJpaAuditing
 @SpringBootApplication
 public class RealcartApplication {
