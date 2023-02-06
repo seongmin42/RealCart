06972e96f2340839eb35eb6822ac5e891da557c6
문성현 <forest7592@hanmail.net>
2023-02-06 18:05:45 +0900

    Misc: Add view to mainpage
    Former-commit-id: ef90c421709b9e0ada94041a42fcccdbdbd45907 [formerly bd538324ece8d836788a94408caaaa071b40a687]
    Former-commit-id: 6bd6cdabb6fda7983a922f3902409e7b924b661d

--- Frontend/src/pages/MainPage.jsx
+++ Frontend/src/pages/MainPage.jsx
@@ -1,12 +1,223 @@
-import React from "react";
+/* eslint-disable */
+import React, { useRef, useEffect } from "react";
 import { Box, Paper } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import { DataGrid } from "@mui/x-data-grid";
-import { useNavigate } from "react-router-dom";
-// import car from "../assets/car.jpg";
+// import { useNavigate } from "react-router-dom";
+import kurentoUtils from "kurento-utils";
+import Stomp from "stompjs";
 
 function MainPage() {
-  const navigate = useNavigate();
+  var ws = new WebSocket("wss://13.125.13.39:8090/call");
+  var socket = new WebSocket("wss://13.125.13.39:8090/chat");
+  var stompClient;
+  var stompClient = Stomp.over(socket);
+  stompClient.connect({}, function () {
+    stompClient.subscribe("/subscribe", function (greeting) {
+      console.log(greeting.body);
+    });
+  });
+  // useEffect(() => {
+  //   connect();
+  // }, []);
+  var video = useRef(null);
+  var text = useRef(null);
+  var webRtcPeer;
+  var mediaId;
+
+  window.onload = function () {
+    connect();
+    // video = document.getElementById("video");
+    // text = document.getElementById("text");
+  };
+
+  window.onbeforeunload = function () {
+    ws.close();
+  };
+
+  ws.onmessage = function (message) {
+    var parsedMessage = JSON.parse(message.data);
+    console.info("Received message: " + message.data);
+
+    switch (parsedMessage.id) {
+      case "presenterResponse":
+        presenterResponse(parsedMessage);
+        break;
+      case "viewerResponse":
+        viewerResponse(parsedMessage);
+        break;
+      case "iceCandidate":
+        webRtcPeer.addIceCandidate(parsedMessage.candidate, function (error) {
+          if (error) return console.error("Error adding candidate: " + error);
+        });
+        break;
+      case "stopCommunication":
+        dispose();
+        break;
+      default:
+        console.error("Unrecognized message", parsedMessage);
+    }
+  };
+
+  function presenterResponse(message) {
+    if (message.response != "accepted") {
+      var errorMsg = message.message ? message.message : "Unknow error";
+      console.info("Call not accepted for the following reason: " + errorMsg);
+      dispose();
+    } else {
+      webRtcPeer.processAnswer(message.sdpAnswer, function (error) {
+        if (error) return console.error(error);
+      });
+    }
+  }
+
+  function connect() {
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function () {
+      stompClient.subscribe("/subscribe", function (greeting) {
+        console.log(greeting.body);
+      });
+    });
+  }
+
+  function sendChat() {
+    stompClient.send(
+      "/publish/messages",
+      {},
+      JSON.stringify({
+        message: text.current.value,
+        senderId: 7,
+        receiverId: 14,
+      })
+    );
+  }
+
+  function viewerResponse(message) {
+    if (message.response != "accepted") {
+      var errorMsg = message.message ? message.message : "Unknow error";
+      console.info("Call not accepted for the following reason: " + errorMsg);
+      dispose();
+    } else {
+      console.log("webrtcpeer", webRtcPeer);
+      webRtcPeer.processAnswer(message.sdpAnswer, function (error) {
+        if (error) return console.error(error);
+      });
+    }
+  }
+
+  function presenter(num) {
+    if (!webRtcPeer) {
+      // showSpinner(video);
+    }
+    var options = {
+      localVideo: video.current,
+      onicecandidate: onIceCandidate,
+    };
+    mediaId = num;
+    webRtcPeer = new kurentoUtils.WebRtcPeer.WebRtcPeerSendonly(
+      options,
+      function (error) {
+        if (error) {
+          return console.error(error);
+        }
+        webRtcPeer.generateOffer(onOfferPresenter);
+      }
+    );
+  }
+
+  function onOfferPresenter(error, offerSdp) {
+    if (error) return console.error("Error generating the offer");
+    console.info("Invoking SDP offer callback function " + mediaId);
+    var message = {
+      id: "presenter",
+      sdpOffer: offerSdp,
+      mediaId: mediaId,
+    };
+    sendMessage(message);
+  }
+
+  function viewer(num) {
+    if (!webRtcPeer) {
+      // showSpinner(video);
+    }
+    mediaId = num;
+    console.log(num);
+    var options = {
+      remoteVideo: video.current,
+      onicecandidate: onIceCandidate,
+    };
+    webRtcPeer = new kurentoUtils.WebRtcPeer.WebRtcPeerRecvonly(
+      options,
+      function (error) {
+        if (error) {
+          return console.error(error);
+        }
+        this.generateOffer(onOfferViewer);
+      }
+    );
+  }
+
+  function onOfferViewer(error, offerSdp) {
+    if (error) return console.error("Error generating the offer");
+    console.info("Invoking SDP offer callback function " + mediaId);
+    var message = {
+      id: "viewer",
+      sdpOffer: offerSdp,
+      mediaId: mediaId,
+    };
+    sendMessage(message);
+  }
+
+  function onIceCandidate(candidate) {
+    console.log("Local candidate" + JSON.stringify(candidate));
+
+    var message = {
+      id: "onIceCandidate",
+      candidate: candidate,
+      mediaId: mediaId,
+    };
+    sendMessage(message);
+  }
+
+  function stop() {
+    var message = {
+      id: "stop",
+    };
+    sendMessage(message);
+    dispose();
+  }
+
+  function dispose() {
+    if (webRtcPeer) {
+      webRtcPeer.dispose();
+      webRtcPeer = null;
+    }
+    // hideSpinner(video);
+  }
+
+  function sendMessage(message) {
+    var jsonMessage = JSON.stringify(message);
+    console.log("Sending message: " + jsonMessage);
+    ws.send(jsonMessage);
+  }
+
+  function showSpinner() {
+    for (var i = 0; i < arguments.length; i++) {
+      arguments[i].poster = "./img/transparent-1px.png";
+      arguments[i].style.background =
+        'center transparent url("./img/spinner.gif") no-repeat';
+    }
+  }
+
+  function hideSpinner() {
+    for (var i = 0; i < arguments.length; i++) {
+      arguments[i].src = "";
+      arguments[i].poster = "./img/webrtc.png";
+      arguments[i].style.background = "";
+    }
+  }
+
+  // const navigate = useNavigate();
 
   const columns = [
     { field: "id", headerName: "순위", width: 150 },
@@ -83,20 +294,93 @@ function MainPage() {
               alignItems: "center",
             }}
           >
-            <Box
-              component="img"
-              alt="mainspect"
-              src="http://192.168.83.21:8080/?action=stream"
-              sx={{
-                width: "90%",
-                height: "90%",
-                cursor: "pointer",
-                transform: "rotate(180deg)",
-              }}
-              onClick={() => {
-                navigate("/spect");
-              }}
-            />
+            <div>
+              <div className="row">
+                <div className="col-md-5">
+                  <div className="row">
+                    <div className="col-md-12">
+                      <button
+                        onClick={() => {
+                          presenter(1);
+                        }}
+                        id="presenter1"
+                        href="#"
+                        className="btn btn-success"
+                      >
+                        <span className="glyphicon glyphicon-play"></span>{" "}
+                        Presenter1{" "}
+                      </button>
+                      <button
+                        onClick={() => {
+                          presenter(2);
+                        }}
+                        id="presenter2"
+                        href="#"
+                        className="btn btn-success"
+                      >
+                        <span className="glyphicon glyphicon-play"></span>{" "}
+                        Presenter2{" "}
+                      </button>
+                      <button
+                        onClick={() => {
+                          presenter(3);
+                        }}
+                        id="presenter3"
+                        href="#"
+                        className="btn btn-success"
+                      >
+                        <span className="glyphicon glyphicon-play"></span>{" "}
+                        Presenter3{" "}
+                      </button>
+                      <button
+                        onClick={() => {
+                          viewer(1);
+                        }}
+                        id="viewer"
+                        href="#"
+                        className="btn btn-primary"
+                      >
+                        <span className="glyphicon glyphicon-user"></span>{" "}
+                        Viewer1
+                      </button>
+                      <button
+                        onClick={() => {
+                          viewer(2);
+                        }}
+                        id="viewer"
+                        href="#"
+                        className="btn btn-primary"
+                      >
+                        <span className="glyphicon glyphicon-user"></span>{" "}
+                        Viewer2
+                      </button>
+                      <button
+                        onClick={() => {
+                          viewer(3);
+                        }}
+                        id="viewer"
+                        href="#"
+                        className="btn btn-primary"
+                      >
+                        <span className="glyphicon glyphicon-user"></span>{" "}
+                        Viewer3
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-7">
+                  <div id="videoBig">
+                    <video
+                      ref={video}
+                      id="video"
+                      autoPlay
+                      width="640px"
+                      height="480px"
+                    ></video>
+                  </div>
+                </div>
+              </div>
+            </div>
           </Paper>
         </Box>
         <Box
