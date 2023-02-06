c0a65163d397c6508078f088fccfb528691ac654
unknown <seongmin42@naver.com>
2023-02-06 14:29:25 +0900

    Misc
    Former-commit-id: 57ffae703977f801579b20bfc666db563b64bbf9 [formerly 8a1255e7a7886c261c4ab90c2321fd890a0c7e8f]
    Former-commit-id: e880a4e93fcefb87bdcfa17fc29230b40676af7a

--- Frontend/src/pages/PlayPage.jsx
+++ Frontend/src/pages/PlayPage.jsx
@@ -10,10 +10,7 @@ function PlayPage() {
 
   const ws = new WebSocket("ws://i8a403.p.ssafy.io:8581");
 
-  ws.onopen = function open() {
-    console.log("connected");
-    ws.send(Date.now());
-  };
+  // ws.onopen = function open() { };
 
   ws.onclose = function close() {
     console.log("disconnected");
