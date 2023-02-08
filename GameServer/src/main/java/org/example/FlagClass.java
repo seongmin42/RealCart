51ced37b19bea9e20b732ecc2163718f48f945e6
unknown <seongmin42@naver.com>
2023-02-08 15:43:13 +0900

    Feat: Add game features
    Former-commit-id: 6a30462640679aed9661b4e7672ccc8a8d778502 [formerly 15cc5e9d1b69245ea9ee4555fd4e7a216a571079]
    Former-commit-id: a6ffc70a197c5e7490bab69da6ba47c019a1b967

--- GameServer/src/main/java/org/example/FlagClass.java
+++ GameServer/src/main/java/org/example/FlagClass.java
@@ -1,28 +1,53 @@
 package org.example;
 
-import org.java_websocket.WebSocket;
-
-import java.util.List;
-
 public class FlagClass {
-    private int player1status;
-    private int player2status;
+    private String player1Nickname;
+    private String player2Nickname;
+    private int player1Status;
+    private int player2Status;
+    private int car1Status;
+    private int car2Status;
     private int gameStatus;
+    private Long startTime;
 
-    public int getPlayer1status() {
-        return player1status;
+
+    private FlagClass(){}
+    private static FlagClass instance = new FlagClass();
+    public static FlagClass getInstance(){
+        return instance;
     }
 
-    public synchronized void setPlayer1status(int player1status) {
-        this.player1status = player1status;
+
+    public String getPlayer1Nickname() {
+        return player1Nickname;
     }
 
-    public int getPlayer2status() {
-        return player2status;
+    public synchronized void setPlayer1Nickname(String player1Nickname) {
+        this.player1Nickname = player1Nickname;
     }
 
-    public synchronized void setPlayer2status(int player2status) {
-        this.player2status = player2status;
+    public String getPlayer2Nickname() {
+        return player2Nickname;
+    }
+
+    public synchronized void setPlayer2Nickname(String player2Nickname) {
+        this.player2Nickname = player2Nickname;
+    }
+
+    public int getPlayer1Status() {
+        return player1Status;
+    }
+
+    public synchronized void setPlayer1Status(int player1Status) {
+        this.player1Status = player1Status;
+    }
+
+    public int getPlayer2Status() {
+        return player2Status;
+    }
+
+    public synchronized void setPlayer2Status(int player2Status) {
+        this.player2Status = player2Status;
     }
 
     public int getGameStatus() {
@@ -31,5 +56,37 @@ public class FlagClass {
 
     public synchronized void setGameStatus(int gameStatus) {
         this.gameStatus = gameStatus;
+    }
+
+    public int getCar1Status() {
+        return car1Status;
+    }
+
+    public synchronized void setCar1Status(int car1Status) {
+        this.car1Status = car1Status;
+    }
+
+    public int getCar2Status() {
+        return car2Status;
+    }
+
+    public synchronized void setCar2Status(int car2Status) {
+        this.car2Status = car2Status;
+    }
+
+    public Long getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Long startTime) {
+        this.startTime = startTime;
+    }
+
+    public void initiateAll() {
+        this.gameStatus = 0;
+        this.player1Status = 0;
+        this.player2Status = 0;
+        this.car1Status = 0;
+        this.car2Status = 0;
     }
 }
--- GameServer/src/main/java/org/example/Test.java
+++ GameServer/src/main/java/org/example/Test.java
@@ -2,11 +2,37 @@ package org.example;
 
 import com.google.gson.Gson;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Test {
-    public static void main(String[] args) {
-        String jsonData = "{\"timestamp\" : 1675741831676, \"speed\" : 56, \"gateNo\" : 3, \"status\" : 2}";
-        Gson gson = new Gson();
-        RcCarStatus rcCarStatus = gson.fromJson(jsonData, RcCarStatus.class);
-        System.out.println(rcCarStatus);
+
+    public static void main(String[] args) throws Exception{
+        String url = "http://127.0.0.1:8060/game/result";
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        String postData = "";
+
+        con.setRequestMethod("POST");
+        con.setDoOutput(true);
+        con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        con.setRequestProperty("Content-Length", Integer.toString(postData.length()));
+        con.setUseCaches(false);
+
+        try (DataOutputStream dos = new DataOutputStream(con.getOutputStream())) {
+            dos.writeBytes(postData);
+        }
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(
+                con.getInputStream())))
+        {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
     }
 }
