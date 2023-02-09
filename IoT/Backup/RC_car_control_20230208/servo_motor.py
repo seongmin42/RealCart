8c480f3431fafb3bb0ca591ed986bc2d8f6683e3
Jeong UiGwon <jek9412@naver.com>
2023-02-09 16:24:27 +0900

    Feat: Update Qt GUI
    added Qt GUI
    
    Related to: S08P12A403-207
    
    
    Former-commit-id: 6344650a6ce9c1815ae46c494b52ede952856a1a [formerly e06104db701313263ee29bf9f9085b62323229fc]
    Former-commit-id: adc9e75afd2954f8a9ee0add992105652ba7e0cd

--- IoT/Backup/RC_car_control_20230208/servo_motor.py
+++ IoT/Backup/RC_car_control_20230208/servo_motor.py
@@ -10,16 +10,16 @@ class SERVO_MOTOR:
             GPIO.setup(servo_pin, GPIO.OUT)
             
             self.pwm = GPIO.PWM(servo_pin, 50)
-            self.error = 0
             self.pwm.start(7.75)
             time.sleep(0.3)
-                        
-        except Exception as e:
-            self.error = 1
-            print('SERVO MOTOR ERROR :', e)
+            
+            print('servo_motor GPIO pin setting complete')
+            
+        except:
+            print('servo_motor GPIO pin setting failed')
     
-    def steering(self, direction):
-        
+    def steering(self, direction, flag_handling):
+        print('flag_handling :', flag_handling)
         try:
             if (direction == 'center'):
                 self.pwm.ChangeDutyCycle(7.75)
@@ -28,10 +28,12 @@ class SERVO_MOTOR:
             elif (direction == 'left'):
                 self.pwm.ChangeDutyCycle(5.5)
                 time.sleep(0.3)
+                print('left')
                 
             elif (direction == 'right'):
                 self.pwm.ChangeDutyCycle(9)
                 time.sleep(0.3)
+                print('right')
             
             flag_handling = False
         
