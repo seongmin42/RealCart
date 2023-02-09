d6f7b657136b2075aca49255e7cbbe6e1ff8358c
SangWoo <brianpsw@naver.com>
2023-02-09 13:02:50 +0900

    Feat: Add user game history feature show user's game history and update some game feature Related to: None
    Former-commit-id: 8a63a5b0aedf059a0a2d568b68c2c37be78679ef [formerly 521b6a8c07496dd516d705835e39794b41b24f75]
    Former-commit-id: 4c11403c38fc7305b25ce655ea77c465d9bf0ed1

--- Backend/src/main/java/com/ssafy/realcart/service/RecordService.java
+++ Backend/src/main/java/com/ssafy/realcart/service/RecordService.java
@@ -10,9 +10,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ssafy.realcart.data.dao.inter.IPlayDAO;
 import com.ssafy.realcart.data.dao.inter.IRecordDAO;
 import com.ssafy.realcart.data.dao.inter.IUserDAO;
+import com.ssafy.realcart.data.dto.PlayDto;
+import com.ssafy.realcart.data.dto.PlayResponseDto;
 import com.ssafy.realcart.data.dto.RecordDto;
+import com.ssafy.realcart.data.entity.Game;
+import com.ssafy.realcart.data.entity.Play;
 import com.ssafy.realcart.data.entity.Record;
 import com.ssafy.realcart.data.entity.User;
 import com.ssafy.realcart.service.inter.IRecordService;
@@ -21,13 +26,15 @@ import com.ssafy.realcart.service.inter.IRecordService;
 public class RecordService implements IRecordService {
 
 	private IRecordDAO recordDAO;
+	private IPlayDAO playDAO;
 	private IUserDAO userDAO;
     private final Logger LOGGER = LoggerFactory.getLogger(RecordService.class);
 
     @Autowired
-    public RecordService(IRecordDAO recordDAO, IUserDAO userDAO){
+    public RecordService(IRecordDAO recordDAO, IUserDAO userDAO, IPlayDAO playDAO){
         this.recordDAO = recordDAO;
         this.userDAO = userDAO;
+        this.playDAO = playDAO;
     }
 
 	@Override
@@ -38,7 +45,12 @@ public class RecordService implements IRecordService {
 		int rank = 1;
 		for (Record record : list) {
 			RecordDto recordDto = new RecordDto();
-			recordDto.setLapTime(sdf.format(new Date(record.getLapTime())));
+			if(record.getLapTime() == Long.MAX_VALUE) {
+				recordDto.setLapTime("기권");
+			}
+			else {
+				recordDto.setLapTime(sdf.format(new Date(record.getLapTime())));
+			}
 			recordDto.setNickname(record.getUser().getNickname());
 			recordDto.setRank(rank++);
 			dtolist.add(recordDto);
@@ -47,7 +59,46 @@ public class RecordService implements IRecordService {
 	}
 
 	@Override
-	public RecordDto getUserRecord(String nickname) {
+	public List<PlayResponseDto> getUserRecord(String nickname) {
+		User user = userDAO.checkNickname(nickname);
+		if(user == null) {
+			return null;
+		}
+		List<Play> list = playDAO.getAllPlay(user.getUserId());
+		SimpleDateFormat sdf = new SimpleDateFormat("mm:ss.SSS");
+		List<PlayResponseDto> answer = new ArrayList<PlayResponseDto>();
+		for (Play play : list) {
+			PlayResponseDto playResponseDto = new PlayResponseDto();
+			Game game = play.getGame();
+			playResponseDto.setGameId(game.getId());
+			playResponseDto.setGameTime(game.getCreatedDate());
+			List<Play> gamelist = playDAO.getPlay(game.getId());
+			for (Play play2 : gamelist) {
+				if(!play2.getUser().getNickname().equals(nickname)) {
+					playResponseDto.setOppo(play2.getUser().getNickname());
+					if(play2.getLapTime() == Long.MAX_VALUE) {
+						playResponseDto.setOppoLapTime("기권");
+					}
+					else {
+						playResponseDto.setOppoLapTime(sdf.format(new Date(play2.getLapTime())));
+					}
+				}
+			}
+			playResponseDto.setIsWin(play.getIsWin());
+			if(play.getLapTime() == Long.MAX_VALUE) {
+				playResponseDto.setLapTime("기권");
+			}
+			else {
+				playResponseDto.setLapTime(sdf.format(new Date(play.getLapTime())));
+			}
+			playResponseDto.setNickname(nickname);
+			answer.add(playResponseDto);
+		}
+		return answer;
+	}
+
+	@Override
+	public RecordDto getBestRecord(String nickname) {
 		List<RecordDto> list = getRecord();
 		for (RecordDto recordDto : list) {
 			if(recordDto.getNickname().equals(nickname)) {
