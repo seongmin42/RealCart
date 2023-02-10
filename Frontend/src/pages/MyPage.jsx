a7c58eb531fd9b1e9bdcc54692df0cf805ce4497
박소정 <sjcoder9611@naver.com>
2023-02-10 16:53:06 +0900

    Feat: Add boards in mainpage
    Consist of component of table
    
    Resolves: #S08P12A403-253, #S08P12A403-256, #S08P12A403-243
    
    Former-commit-id: 448f6cecdf496922dc81650f3936d274779fb006 [formerly 4be6486f33e7f68787f495950b6e0264e45719cd]
    Former-commit-id: b1965c5ec5053b64ac16c8ac41704ae1670b4bd5

--- Frontend/src/pages/MyPage.jsx
+++ Frontend/src/pages/MyPage.jsx
@@ -1,40 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Box, Divider, Paper, Grid } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
+import RecordTable from "../components/RecordTable";
 
 function MyPage() {
-  const columns = [
-    { field: "id", headerName: "순위", width: 150 },
-    { field: "nickname", headerName: "NickName", width: 150, editable: true },
-    { field: "laptime", headerName: "LapTime", width: 150, editable: true },
-    { field: "date", headerName: "Date", width: 150, editable: true },
-  ];
-  const ranking = [];
-  ranking.push({
-    id: 1,
-    nickname: "의권짱짱123",
-    laptime: "01:23:59",
-    date: "2023.01.13",
-  });
-  ranking.push({
-    id: 2,
-    nickname: "v스피드왕번개v",
-    laptime: "01:23:59",
-    date: "2023.01.13",
-  });
-  const history = [];
-  history.push({
-    id: 1,
-    nickname: "의권짱짱123",
-    laptime: "01:23:59",
-    date: "2023.01.13",
-  });
-  history.push({
-    id: 2,
-    nickname: "의권짱짱123",
-    laptime: "01:24:59",
-    date: "2023.01.13",
-  });
+  const user = useSelector((state) => state.login.user);
+
   return (
     <Box
       display="flex"
@@ -50,36 +21,17 @@ function MyPage() {
           <Divider />
           <Paper>
             <Box>
-              <h1>실시간 랭킹</h1>
+              <h1>랭킹 & 최고기록</h1>
             </Box>
-            <Box sx={{ height: 400, width: 1500 }} justifyItems="center">
-              <DataGrid
-                rows={ranking}
-                columns={columns}
-                pageSize={5}
-                rowsPerPageOptions={[5]}
-                experimentalFeatures={{ newEditingApi: true }}
-              />
-            </Box>
+            <RecordTable address={`best/record/${user}`} />
           </Paper>
         </Grid>
         <br />
         <Grid item xs={12}>
           <Divider />
-          <Paper>
-            <Box>
-              <h1>히스토리</h1>
-            </Box>
-            <Box sx={{ height: 400, width: 1500 }} justifyItems="center">
-              <DataGrid
-                rows={history}
-                columns={columns}
-                pageSize={5}
-                rowsPerPageOptions={[5]}
-                experimentalFeatures={{ newEditingApi: true }}
-              />
-            </Box>
-          </Paper>
+          <Box>
+            <h1>히스토리</h1>
+          </Box>
         </Grid>
       </Grid>
     </Box>
