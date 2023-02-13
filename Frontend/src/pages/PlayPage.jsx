1a33bb645ea9f3c4750b251f52359c44cda9783b
unknown <seongmin42@naver.com>
2023-02-13 13:33:35 +0900

    Fix: Revise Key send with 0.1s
    Former-commit-id: 988490b5558d2bc8ff3d0ff6a2f5a9fc69105c5d [formerly 69668428fec3b0ec14bbd3cad651deb7d6328104]
    Former-commit-id: d991da034817c8ee088bcfd30f2391e1dff56e31

--- Frontend/src/pages/PlayPage.jsx
+++ Frontend/src/pages/PlayPage.jsx
@@ -611,7 +611,7 @@ function PlayPage() {
       const interval = setInterval(() => {
         console.log("stop");
         wss.send(16);
-      }, 10);
+      }, 100);
       setTimeout(() => {
         clearInterval(interval);
         setInputSwitch((prevState) => ({
@@ -628,7 +628,7 @@ function PlayPage() {
       const interval = setInterval(() => {
         console.log("ctrl");
         wss.send(17);
-      }, 10);
+      }, 100);
       setTimeout(() => {
         clearInterval(interval);
         setInputSwitch((prevState) => ({
@@ -645,7 +645,7 @@ function PlayPage() {
       const interval = setInterval(() => {
         console.log("left");
         wss.send(37);
-      }, 10);
+      }, 100);
       setTimeout(() => {
         clearInterval(interval);
         setInputSwitch((prevState) => ({
@@ -662,7 +662,7 @@ function PlayPage() {
       const interval = setInterval(() => {
         console.log("up");
         wss.send(38);
-      }, 10);
+      }, 100);
       setTimeout(() => {
         clearInterval(interval);
         setInputSwitch((prevState) => ({
@@ -679,7 +679,7 @@ function PlayPage() {
       const interval = setInterval(() => {
         console.log("right");
         wss.send(39);
-      }, 10);
+      }, 100);
       setTimeout(() => {
         clearInterval(interval);
         setInputSwitch((prevState) => ({
@@ -696,7 +696,7 @@ function PlayPage() {
       const interval = setInterval(() => {
         console.log("down");
         wss.send(40);
-      }, 10);
+      }, 100);
       setTimeout(() => {
         clearInterval(interval);
         setInputSwitch((prevState) => ({
