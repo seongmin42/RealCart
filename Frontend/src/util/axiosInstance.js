6284da72f6aefda4b4b0ad699eba3b37b5fc9dcb
unknown <seongmin42@naver.com>
2023-02-16 12:52:28 +0900

    Fix: Revise axios interceptor code
    Former-commit-id: 38c537755ddc561f50a908a841e35e11908069a8 [formerly e1244eee03cdce4d956c28fb970ecbe07b707044]
    Former-commit-id: 6132a2d6d56624db172036d8b63b4c974e4c15b6

--- Frontend/src/util/axiosInstance.js
+++ Frontend/src/util/axiosInstance.js
@@ -1,22 +1,21 @@
 import axios from "axios";
 
-axios.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("access-token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    if (error.response.status === 401) {
-      axios
-        .get(`${process.env.REACT_APP_BACKEND_URL}/accounts/auth/refresh`)
-        .catch((err) => {
-          console.log(err);
-        });
-    }
+axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access-token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
-);
+  return config;
+});
+
+axios.interceptors.response.use((error) => {
+  if (error.response.status === 401) {
+    axios
+      .get(`${process.env.REACT_APP_BACKEND_URL}/accounts/auth/refresh`)
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+});
 
 export default axios;
